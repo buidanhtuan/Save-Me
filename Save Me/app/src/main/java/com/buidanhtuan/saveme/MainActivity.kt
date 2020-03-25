55afb54ce5e713dd656ea3ed722c64f8376a3bf2
17021101 <43320619+buidanhtuan@users.noreply.github.com>
2020-03-25 10:54:44 +0700

    update gridview

--- Save Me/app/src/main/java/com/buidanhtuan/saveme/MainActivity.kt
+++ Save Me/app/src/main/java/com/buidanhtuan/saveme/MainActivity.kt
@@ -3,6 +3,8 @@ package com.buidanhtuan.saveme
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.AdapterView
+import android.widget.GridView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
@@ -11,8 +13,8 @@ import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-
-
+    private val itemList: Array<String>
+        get() = arrayOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10", "Item 11", "Item 12", "Item 13", "Item 14", "Item 15", "Item 16", "Item 17", "Item 18", "Item 19", "Item 20", "Item 21", "Item 22")
     lateinit var toolbar: Toolbar
     lateinit var drawerLayout: DrawerLayout
     lateinit var navView: NavigationView
@@ -32,6 +34,17 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         navView.setNavigationItemSelectedListener(this)
+
+        val gridview = findViewById<GridView>(R.id.gridview)
+
+        val adapter = NoteAdapter(this, R.layout.adapter_note, itemList)
+        gridview.adapter = adapter
+
+        gridview.onItemClickListener = AdapterView.OnItemClickListener { parent, v, position, id ->
+            Toast.makeText(this@MainActivity, " Clicked Position: " + (position + 1),
+                Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
---
+++ Save Me/app/src/main/java/com/buidanhtuan/saveme/NoteAdapter.kt
@@ -0,0 +1,38 @@
+package com.buidanhtuan.saveme
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+internal class NoteAdapter internal constructor(context: Context, private val resource: Int, private val itemList: Array<String>?) : ArrayAdapter<NoteAdapter.ItemHolder>(context, resource) {
+
+    override fun getCount(): Int {
+        return if (this.itemList != null) this.itemList.size else 0
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var convertView = convertView
+
+        val holder: ItemHolder
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(resource, null)
+            holder = ItemHolder()
+            holder.name = convertView!!.findViewById(R.id.textView)
+            convertView.tag = holder
+        } else {
+            holder = convertView.tag as ItemHolder
+        }
+
+        holder.name!!.text = this.itemList!![position]
+
+        return convertView
+    }
+
+    internal class ItemHolder {
+        var name: TextView? = null
+    }
+}
